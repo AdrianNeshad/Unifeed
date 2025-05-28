dc9d3f33a6055c9bf63a69a980e81a4c0406af62
AdrianNeshad <Adrian.neshad1@gmail.com>
2025-05-28 19:05:56 +0200

    Initial Commit

---
+++ Unifeed/UnifeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnifeedApp.swift
+//  Unifeed
+//
+//  Created by Adrian Neshad on 2025-05-28.
+//
+
+import SwiftUI
+
+@main
+struct UnifeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
